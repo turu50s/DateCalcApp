17b05f076de2d69ad5c3ed76838ddd6f422b6c30
tsuruki <sigeru0216@gmail.com>
2019-01-16 16:09:13 +0900

    DateCalcAppApplictionTestsに@TestPropertySourceを追加

--- src/test/java/com/tsuruki/spring/DateCalcAppApplicationTests.java
+++ src/test/java/com/tsuruki/spring/DateCalcAppApplicationTests.java
@@ -3,15 +3,17 @@ package com.tsuruki.spring;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@TestPropertySource(locations = "classpath:test.properties")
 public class DateCalcAppApplicationTests {
 
-//	@Test
-//	public void contextLoads() {
-//	}
+	@Test
+	public void contextLoads() {
+	}
 
 }
 
