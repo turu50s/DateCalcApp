712051f29ed6966f49e7bf22991c3a5d1bb17d36
tsuruki <sigeru0216@gmail.com>
2019-01-16 16:17:25 +0900

    TestProperrySourceを削除

--- src/test/java/com/tsuruki/spring/DateCalcAppApplicationTests.java
+++ src/test/java/com/tsuruki/spring/DateCalcAppApplicationTests.java
@@ -8,7 +8,6 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-@TestPropertySource(locations = "classpath:test.properties")
 public class DateCalcAppApplicationTests {
 
 	@Test
